66e5c769f8e94b468b38db5eb3a11da5fb43cf1e
Julian Ezequiel Mora Arcagni <78808464+Jul-m2323@users.noreply.github.com>
2022-04-20 00:48:45 -0300

    Commit con arreglos

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ const proyectosId = document.getElementById('proyectos');
 
 
 const proyectos = [
-    {img: "./multimedia/paint-app.png",pag:"https://jul-m2323.github.io/DrawingApp_JS/", name: "Paint App"},
-    {img: "./multimedia/quiz-app.png",pag:"https://jul-m2323.github.io/QuizAppJS/", name: "Quiz App"},
-    {img: "./multimedia/slappy-diy-shop.png",pag:"https://jul-m2323.github.io/slappy_DIY_mora/shop.html", name: "Slappy DIY Shop"},
-    {img: "./multimedia/todo-app.png",pag:"https://jul-m2323.github.io/Todo-appJS/", name: "Todo App"}
+    {img: "multimedia/paint-app.png",pag:"https://jul-m2323.github.io/DrawingApp_JS/", name: "Paint App"},
+    {img: "multimedia/quiz-app.png",pag:"https://jul-m2323.github.io/QuizAppJS/", name: "Quiz App"},
+    {img: "multimedia/slappy-diy-shop.png",pag:"https://jul-m2323.github.io/slappy_DIY_mora/shop.html", name: "Slappy DIY Shop"},
+    {img: "multimedia/todo-app.png",pag:"https://jul-m2323.github.io/Todo-appJS/", name: "Todo App"}
 ]
 
 for (element of proyectos){
